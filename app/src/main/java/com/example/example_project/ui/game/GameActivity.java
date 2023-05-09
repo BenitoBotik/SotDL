8e175bec3708062b06636742d3ea6cf9f6ae988e
benito-botik <ben.ledenev@gmail.com>
2023-05-09 17:22:35 +0300

    saves icons in firestore perfectly! no bugs

--- app/src/main/java/com/example/example_project/ui/game/GameActivity.java
+++ app/src/main/java/com/example/example_project/ui/game/GameActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -56,34 +57,14 @@ public class GameActivity extends AppCompatActivity {
         // loop through list of image objects and create ImageView for each one
         if (icons != null) {
             for (Icon icon : icons) {
-                ImageView imageView = new ImageView(this);
-                int resourceId = getResources().getIdentifier(icon.getImage(), "drawable", getPackageName());
-                imageView.setImageResource(resourceId);
-
-                // set layout params for image view
-                ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-                        ConstraintLayout.LayoutParams.WRAP_CONTENT,
-                        ConstraintLayout.LayoutParams.WRAP_CONTENT
-                );
-                layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-                layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-                layoutParams.horizontalBias = icon.getX() / ConstraintLayout.LayoutParams.MATCH_PARENT;
-                layoutParams.verticalBias = icon.getY() / ConstraintLayout.LayoutParams.MATCH_PARENT;
-
-                // add image view to layout
-                ConstraintLayout layout = findViewById(R.id.my_parent_layout);
-                layout.addView(imageView, layoutParams);
-            }
-        }
-
-        // add a movable icon to game layout
-        addIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // create a new game icon of size 64x64
                 ImageView currentIcon = new ImageView(GameActivity.this);
-                currentIcon.setImageResource(R.drawable.icon3);
+                int resourceId = getResources().getIdentifier(icon.getImage(), "drawable", getPackageName());
+                currentIcon.setImageResource(resourceId);
                 currentIcon.setLayoutParams(new ViewGroup.LayoutParams(64, 64));
+
+                // position icon in the correct location
+                currentIcon.setX(icon.getX());
+                currentIcon.setY(icon.getY());
 
                 // Set the ImageView's id
                 currentIcon.setId(View.generateViewId()); // generate a unique id for the ImageView
@@ -91,62 +72,82 @@ public class GameActivity extends AppCompatActivity {
                 // add the game icon to the main layout
                 gameLayout.addView(currentIcon);
 
-                // Center the ImageView in the parent ConstraintLayout
-                ConstraintSet constraintSet = new ConstraintSet();
-                constraintSet.clone(gameLayout);
-                constraintSet.centerHorizontally(currentIcon.getId(), ConstraintSet.PARENT_ID);
-                constraintSet.centerVertically(currentIcon.getId(), ConstraintSet.PARENT_ID);
-                constraintSet.applyTo(gameLayout);
-
                 // Set an OnTouchListener to handle touch events on the ImageView
                 currentIcon.setOnTouchListener(new TouchHandler(findViewById(R.id.my_parent_layout), currentIcon, bucket));
             }
-        });
 
-        //update button is clicked
-        // Set up the button
-        Button button = findViewById(R.id.button_update);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Get the parent layout
-                ConstraintLayout parentLayout = findViewById(R.id.my_parent_layout);
+            // add a movable icon to game layout
+            addIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // create a new game icon of size 64x64
+                    ImageView currentIcon = new ImageView(GameActivity.this);
+                    currentIcon.setImageResource(R.drawable.icon3);
+                    currentIcon.setLayoutParams(new ViewGroup.LayoutParams(64, 64));
 
-                // Create the list to hold the icons
-                ArrayList<Icon> newIcons = new ArrayList<>();
+                    // Set the ImageView's id
+                    currentIcon.setId(View.generateViewId()); // generate a unique id for the ImageView
 
-                // Loop over all the child views
-                for (int i = 0; i < parentLayout.getChildCount(); i++) {
-                    View childView = parentLayout.getChildAt(i);
+                    // add the game icon to the main layout
+                    gameLayout.addView(currentIcon);
 
-                    // Skip the two imageviews with the same ids and positions
-                    if (childView.getId() == R.id.imageview_bucket || childView.getId() == R.id.imageview_add_icon || childView.getId() == R.id.id_textview || childView.getId() == R.id.button_update) {
-                        continue;
+                    // Center the ImageView in the parent ConstraintLayout
+                    ConstraintSet constraintSet = new ConstraintSet();
+                    constraintSet.clone(gameLayout);
+                    constraintSet.centerHorizontally(currentIcon.getId(), ConstraintSet.PARENT_ID);
+                    constraintSet.centerVertically(currentIcon.getId(), ConstraintSet.PARENT_ID);
+                    constraintSet.applyTo(gameLayout);
+
+                    // Set an OnTouchListener to handle touch events on the ImageView
+                    currentIcon.setOnTouchListener(new TouchHandler(findViewById(R.id.my_parent_layout), currentIcon, bucket));
+                }
+            });
+
+            //update button is clicked
+            // Set up the button
+            Button button = findViewById(R.id.button_update);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Get the parent layout
+                    ConstraintLayout parentLayout = findViewById(R.id.my_parent_layout);
+
+                    // Create the list to hold the icons
+                    ArrayList<Icon> newIcons = new ArrayList<>();
+
+                    // Loop over all the child views
+                    for (int i = 0; i < parentLayout.getChildCount(); i++) {
+                        View childView = parentLayout.getChildAt(i);
+
+                        // Skip the two imageviews with the same ids and positions
+                        if (childView.getId() == R.id.imageview_bucket || childView.getId() == R.id.imageview_add_icon || childView.getId() == R.id.id_textview || childView.getId() == R.id.button_update) {
+                            continue;
+                        }
+
+                        // Get the position of the child view
+                        ConstraintSet constraintSet = new ConstraintSet();
+                        constraintSet.clone(parentLayout);
+                        constraintSet.constrainWidth(childView.getId(), childView.getWidth());
+                        constraintSet.constrainHeight(childView.getId(), childView.getHeight());
+                        constraintSet.connect(childView.getId(), ConstraintSet.TOP, parentLayout.getId(), ConstraintSet.TOP, 0);
+                        constraintSet.connect(childView.getId(), ConstraintSet.LEFT, parentLayout.getId(), ConstraintSet.LEFT, 0);
+                        constraintSet.applyTo(parentLayout);
+
+                        float x = childView.getX();
+                        float y = childView.getY();
+
+                        // Create an icon and add it to the list
+                        Icon icon = new Icon("icon3", x, y);
+                        newIcons.add(icon);
                     }
 
-                    // Get the position of the child view
-                    ConstraintSet constraintSet = new ConstraintSet();
-                    constraintSet.clone(parentLayout);
-                    constraintSet.constrainWidth(childView.getId(), childView.getWidth());
-                    constraintSet.constrainHeight(childView.getId(), childView.getHeight());
-                    constraintSet.connect(childView.getId(), ConstraintSet.TOP, parentLayout.getId(), ConstraintSet.TOP, 0);
-                    constraintSet.connect(childView.getId(), ConstraintSet.LEFT, parentLayout.getId(), ConstraintSet.LEFT, 0);
-                    constraintSet.applyTo(parentLayout);
+                    // update game icons
+                    game.setIcons(newIcons);
 
-                    float x = childView.getX();
-                    float y = childView.getY();
-
-                    // Create an icon and add it to the list
-                    Icon icon = new Icon("icon3", x, y);
-                    newIcons.add(icon);
+                    // update the game in the database
+                    docRef.set(game);
                 }
-
-                // update game icons
-                game.setIcons(newIcons);
-
-                // update the game in the database
-                docRef.set(game);
-            }
-        });
+            });
+        }
     }
 }
