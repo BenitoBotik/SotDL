61d127430a32359427cd65b2d3c684694b31cb68
benito-botik <ben.ledenev@gmail.com>
2023-05-24 08:08:02 +0300

    added worker

---
+++ app/src/main/java/com/example/example_project/ui/model/MyWorker.java
@@ -0,0 +1,19 @@
+package com.example.example_project.ui.model;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWorker extends Worker {
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        return null;
+    }
+}
